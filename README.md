e1e3bbdd58873ffbcad12eb197aea71866b60dda
ianbjohn <ianbjohn@umail.iu.edu>
2018-07-05 02:46:57 -0400

    added roll command

---
+++ README.md
@@ -0,0 +1,9 @@
+# magic-eight-bot-discord
+
+Inspired by a similar bot made for Kik, this is a simple magic eight ball bot I made for a Discord server me and my friends are in.
+https://discordapp.com/
+
+I wrote this as a challenge to get used to working with the Discord API, as well as familiarize myself with Node.js (Used to make the Discord.js module.)
+https://discord.js.org/#/
+
+Me and my friends have had many laughs using it, and it's solved many internal issues / disputes between us.
---
+++ magiceightbot.js
@@ -0,0 +1,62 @@
+//version 1.02, last updated 1/24/17
+//removed the feature I added last version where you HAD to ask it a question
+
+var responses = [
+"It is certain",
+"It is decidedly so",
+"Without a doubt",
+"Yes, definitely",
+"You may rely on it",
+"As I see it, yes",
+"Most likely",
+"Outlook good",
+"Yes",
+"Signs point to yes",
+"Reply hazy try again",
+"Ask again later",
+"Better not tell you now",
+"Cannot predict now",
+"Concentrate and ask again",
+"Don't count on it",
+"My reply is no",
+"My sources say no",
+"Outlook not so good",
+"Very doubtful"];
+
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+client.on("ready", () => {
+	console.log("Boolin");
+})
+
+
+client.on("message", message => {
+	//only respond if the bot was @'d
+	if (message.mentions.users.first() != undefined) {
+		if (message.mentions.users.first().username == "magiceightbot") {
+			//if (message.content.charAt(message.content.length - 1) != '?')
+			//	message.channel.sendMessage("Ask a question dumbass");		//got rid of this feature because its funnier to respond to any message like it's a question
+			//else {
+			switch (Math.floor(Math.random() * 2)) {
+			case 0:
+				message.channel.sendMessage("Let me see...");
+				break;
+			case 1:
+				message.channel.sendMessage("Hmmmmm...");
+				break;
+			}
+			
+			message.channel.sendMessage(responses[Math.floor(Math.random() * responses.length)]);
+			//}
+		}
+	}
+	//roll command
+	else if (message.content == "!roll")
+		message.channel.sendMessage(Math.random() * 1000000000);
+})
+
+
+client.login("MjE5Mjc5Mzc2NjExNjcyMDY1.C0WBmw.BECStpd5u02JWH8kWG_ZL_Kw73A");
